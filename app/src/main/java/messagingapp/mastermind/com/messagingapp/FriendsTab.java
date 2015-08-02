d694433e7cc3e03012bd3dff48fdc75b4236b13b
loftywaif002 <deep.row4@gmail.com>
2015-08-02 02:36:06 -0700

    After Manually Adding new Fragments

---
+++ app/src/main/java/messagingapp/mastermind/com/messagingapp/FriendsTab.java
@@ -0,0 +1,20 @@
+package messagingapp.mastermind.com.messagingapp;
+
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Dip on 8/2/2015.
+ */
+public class FriendsTab extends ListFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_friends, container, false);
+        return rootView;
+    }
+}
---
+++ app/src/main/java/messagingapp/mastermind/com/messagingapp/SectionsPagerAdapter.java
@@ -0,0 +1,60 @@
+package messagingapp.mastermind.com.messagingapp;
+
+/**
+ * Created by Dip on 8/2/2015.
+ */
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.Locale;
+
+/**
+ * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+    Context mContext;
+
+    public SectionsPagerAdapter(Context context,FragmentManager fm) {
+        super(fm);
+        mContext = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        // getItem is called to instantiate the fragment for the given page.
+        // Return a PlaceholderFragment (defined as a static inner class below).
+
+        switch (position){
+
+            case 0:
+                return new InboxTab();
+            case 1:
+                return new FriendsTab();
+        }
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        // Show 2 total pages.
+        return 2;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        Locale l = Locale.getDefault();
+        switch (position) {
+            case 0:
+                return mContext.getString(R.string.title_section1).toUpperCase(l);
+            case 1:
+                return mContext.getString(R.string.title_section2).toUpperCase(l);
+
+        }
+        return null;
+    }
+}
